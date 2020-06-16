040e1900b158d811cc40cc1f3dff18cae4a3b58f
woorui <rui1009479218@gmail.com>
2020-06-16 20:23:22 +0800

    add test dir

---
+++ test-project/index.js
@@ -0,0 +1,6 @@
+const father = require("./father/father");
+const mother = require("./mother/index");
+const child = require("./mother/children/child");
+const sbling = require("./mother/sbling/sbling");
+
+console.log(father, mother, child, sbling);
